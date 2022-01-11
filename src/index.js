81ff7b27ae1f1e5faa1452e7c3eaaff263cd29e9
Mark Nicholson <wmark.nicholsonu@gmail.com>
2022-01-11 15:50:32 +0300

    + 2 columns of links, subtitle, icon

--- src/index.js
+++ src/index.js
@@ -8,15 +8,23 @@ var cardConponent =
         <div className="img">
             <img src="me.jpg" />
         </div>
-        <h2>Mark Nicholson</h2>
+        <div className="name">
+            <h2>Mark Nicholson</h2>
+            <h3>Backend Developer</h3>
+        </div>
         <span>
             <ul>
               <li><a href="mailto:i@justmark0.me" title="E-mail"><i className="fa fa-at" aria-hidden="true" /></a></li>
               <li><a href="https://vk.com/justmark0" title="VK"><i className="fa fa-vk" aria-hidden="true" /></a></li>
               <li><a href="https://t.me/justmark0" title="Telegram"><i className="fa fa-telegram" aria-hidden="true" /></a></li>
-              <li><a href="https://github.com/justmark0" title="GitHub"><i className="fa fa-github" aria-hidden="true" /></a></li>
-              <li><a href="https://www.facebook.com/profile.php?id=100074042946696" title="Facebook"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
-              <li><a href="https://instagram.com/just.mark0" title="Instagram"><i className="fa fa-instagram" aria-hidden="true" /></a></li>
+            </ul>
+        </span>
+        <br/>
+        <span>
+            <ul>
+                <li><a href="https://github.com/justmark0" title="GitHub"><i className="fa fa-github" aria-hidden="true" /></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=100074042946696" title="Facebook"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
+                <li><a href="https://instagram.com/just.mark0" title="Instagram"><i className="fa fa-instagram" aria-hidden="true" /></a></li>
             </ul>
         </span>
     </div>

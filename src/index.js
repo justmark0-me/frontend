de7d5fec4753f71fee8d04b131a72d6f46c3381d
Mark Nicholson <i@justmark0.me>
2024-01-06 23:37:49 +0300

    add statistics by followed links 🙈

--- src/index.js
+++ src/index.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-var cardConponent =
-<div className="card">
-    <div className="box">
-        <div className="img">
-            <img src="me.jpg" />
+const CardComponent = () => {
+    // Function to create event listeners
+    const getListener = (elementID) => {
+        return function () {
+            fetch(`https://api.justmark0.me/v1/new_request/${elementID}`);
+        };
+    };
+
+    // Use useEffect to setup listeners after component mounts
+    useEffect(() => {
+        const elements = ['email', 'linkedin', 'telegram', 'github', 'facebook', 'instagram'];
+        elements.forEach(elementID => {
+            const element = document.getElementById(elementID);
+            if (element) {
+                element.addEventListener('click', getListener(elementID));
+            }
+        });
+
+        // Cleanup listeners when component unmounts
+        return () => {
+            elements.forEach(elementID => {
+                const element = document.getElementById(elementID);
+                if (element) {
+                    element.removeEventListener('click', getListener(elementID));
+                }
+            });
+        };
+    }, []);
+
+    return (
+        <div className="card">
+            <div className="box">
+                <div className="img">
+                    <img src="me.jpg" />
+                </div>
+                <div className="name">
+                    <h2>Mark Nicholson</h2>
+                    <h3>Backend Developer</h3>
+                </div>
+                <span>
+                    <ul>
+                      <li><a href="mailto:i@justmark0.me" title="E-mail"><i className="fa fa-at" aria-hidden="true" id="email" /></a></li>
+                      <li><a href="https://www.linkedin.com/in/justmark0/" title="Linkedin"><i className="fa fa-linkedin" aria-hidden="true" id="linkedin" /></a></li>
+                      <li><a href="https://t.me/justmark0" title="Telegram"><i className="fa fa-telegram" aria-hidden="true" id="telegram" /></a></li>
+                    </ul>
+                </span>
+                <br/>
+                <span>
+                    <ul>
+                        <li><a href="https://github.com/justmark0" title="GitHub"><i className="fa fa-github" aria-hidden="true" id="github" /></a></li>
+                        <li><a href="https://www.facebook.com/profile.php?id=100074042946696" title="Facebook"><i className="fa fa-facebook" aria-hidden="true" id="facebook" /></a></li>
+                        <li><a href="https://instagram.com/just.mark0" title="Instagram"><i className="fa fa-instagram" aria-hidden="true" id="instagram" /></a></li>
+                    </ul>
+                </span>
+            </div>
         </div>
-        <div className="name">
-            <h2>Mark Nicholson</h2>
-            <h3>Backend Developer</h3>
-        </div>
-        <span>
-            <ul>
-              <li><a href="mailto:i@justmark0.me" title="E-mail"><i className="fa fa-at" aria-hidden="true" /></a></li>
-              <li><a href="https://www.linkedin.com/in/justmark0/" title="Linkedin"><i className="fa fa-linkedin" aria-hidden="true" /></a></li>
-              <li><a href="https://t.me/justmark0" title="Telegram"><i className="fa fa-telegram" aria-hidden="true" /></a></li>
-            </ul>
-        </span>
-        <br/>
-        <span>
-            <ul>
-                <li><a href="https://github.com/justmark0" title="GitHub"><i className="fa fa-github" aria-hidden="true" /></a></li>
-                <li><a href="https://www.facebook.com/profile.php?id=100074042946696" title="Facebook"><i className="fa fa-facebook" aria-hidden="true" /></a></li>
-                <li><a href="https://instagram.com/just.mark0" title="Instagram"><i className="fa fa-instagram" aria-hidden="true" /></a></li>
-            </ul>
-        </span>
-    </div>
-</div>;
+    );
+};
 
 ReactDOM.render(
-    cardConponent,
-  document.getElementById('root')
+    <CardComponent />,
+    document.getElementById('root')
 );
